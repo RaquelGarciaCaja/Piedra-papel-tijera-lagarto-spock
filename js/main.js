55fad4509a9a38f73b6e891a85f8556d9dc7cdd9
RaquelGarciaCaja <raquelgc8@gmail.com>
2021-01-05 20:12:48 +0100

    add rules and gif responsive

--- js/main.js
+++ js/main.js
@@ -7,8 +7,7 @@ const btnReset = document.querySelector(".header__buttonReset");
 const computerChoose = document.querySelector(".main__computerLoading");
 const fullResultWin = document.querySelector(".js-fullResultWin");
 const fullResultLose = document.querySelector(".js-fullResultLose");
-const fullResultEqual = document.querySelector(".js-fullResultEqual");
-
+const btnrules = document.querySelector(".header__buttonRules");
 let counter = 0;
 let counterHuman = 0;
 let counterComputer = 0;
@@ -25,12 +24,12 @@ function option(human) {
     [1, 2, 1, 0, 2],
     [2, 1, 2, 1, 0],
   ];
+
   var computer = Math.floor(Math.random() * 5);
   var result = move[computer][human];
   computerChoose.innerHTML = `<button class="main__optionComputer"> ${name[computer]}</button>`;
-  resultWrite.innerHTML = `<h5 class="main__result"> Resultado: ${resultText[result]} </h5>`;
+  resultWrite.innerHTML = `<h3 class="main__result"> Tú: ${name[human]} </h3><h3 class="main__result"> Ordenador: ${name[computer]} </h3><h3 class="main__result"> Resultado: ${resultText[result]} </h3>`;
   counterWrite.innerHTML = `<h4>Partidas: ${counter}</h4>`;
-
   if (resultText[result] === "ganas") {
     counterHuman++;
     counterHumanWrite.innerHTML = `<h4>Ganadas: ${counterHuman}</h4>`;
@@ -43,28 +42,27 @@ function option(human) {
 ////////////counter games/////////////////
 
 function counterTry() {
-  if (counter > 9) {
+  if (counter >= 7) {
     if (counterHuman > counterComputer) {
       fullResultWin.style.display = "flex";
     } else if (counterHuman < counterComputer) {
       fullResultLose.style.display = "flex";
-    } else if (counterHuman === counterComputer) {
-      fullResultEqual.style.display = "flex";
     }
   }
 }
 
-////////////reset/////////////////
+////////////////reset///////////////////////
 
 function reset() {
   location.reload();
 }
 
-////////////close win-lose-equal/////////////////
+////////////close win-lose/////////////////
+
 function close() {
   fullResultWin.style.display = "none";
   fullResultLose.style.display = "none";
-  fullResultEqual.style.display = "none";
+  location.reload();
 }
 
 function closeButtons() {
@@ -75,5 +73,12 @@ function closeButtons() {
 }
 closeButtons();
 
+///////////////////rules/////////////////
+function showRules() {
+  const rules = document.querySelector(".header__rules");
+  rules.classList.toggle("hidden");
+}
+
 /////////////////listeners/////////////////////
 btnReset.addEventListener("click", reset);
+btnrules.addEventListener("click", showRules);
